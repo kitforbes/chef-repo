49f8c6c9ac63d65903a27564a892929284e13f67
Chris Forbes <kitforbes@users.noreply.github.com>
2018-03-18 13:33:13 +0000

    Use README files for long_description in metadata

--- cookbooks/example/metadata.rb
+++ cookbooks/example/metadata.rb
@@ -5,9 +5,15 @@ maintainer 'Chris Forbes'
 maintainer_email 'kitforbes@users.noreply.github.com'
 license 'MIT'
 description 'Installs/Configures example'
-long_description 'Installs/Configures example'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
 
-issues_url 'https://github.com/kitforbes/chef-repo/issues'
+recipe 'example::default', 'Log a name'
+
+supports 'centos'
+supports 'ubuntu'
+
 source_url 'https://github.com/kitforbes/chef-repo'
+issues_url 'https://github.com/kitforbes/chef-repo/issues'
+
+chef_version '>= 12.1' if respond_to?(:chef_version)
--- cookbooks/windows_role/metadata.rb
+++ cookbooks/windows_role/metadata.rb
@@ -5,13 +5,14 @@ maintainer 'Chris Forbes'
 maintainer_email 'kitforbes@users.noreply.github.com'
 license 'MIT'
 description ''
-long_description ''
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
-
-issues_url 'https://github.com/kitforbes/chef-repo/issues'
-source_url 'https://github.com/kitforbes/chef-repo'
 
 supports 'windows'
 
 depends 'windows'
+
+source_url 'https://github.com/kitforbes/chef-repo'
+issues_url 'https://github.com/kitforbes/chef-repo/issues'
+
+chef_version '>= 12.1' if respond_to?(:chef_version)
