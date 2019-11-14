f892b0bb8fdede9574fcb8525a4ca0557d304b7e
Chris Forbes <git@chrisforbes.dev>
2019-11-14 20:46:02 +0000

    Update Git recipe for dynamic SSH config

--- cookbooks/development/recipes/git.rb
+++ cookbooks/development/recipes/git.rb
@@ -1,7 +1,7 @@
 return unless platform_family?('windows')
 
 [
-  'C:\\dev',
+  'C:\dev',
   "C:\\dev\\#{node['git']['username']}",
   "#{ENV['HOME']}\\.ssh",
 ].each do |dir|
@@ -24,8 +24,31 @@ template "#{ENV['HOME']}\\.gitconfig" do
   action :create
 end
 
+file "#{ENV['HOME']}\\.ssh\\known_hosts" do
+  action :create
+end
+
 template "#{ENV['HOME']}\\.ssh\\config" do
   source 'config.erb'
+  variables(
+    hosts: [
+      {
+        host: 'github.com',
+        user: 'git',
+        identityFile: '~/.ssh/github',
+      },
+      {
+        host: 'gitlab.com',
+        user: 'git',
+        identityFile: '~/.ssh/id_rsa',
+      },
+      {
+        host: 'bitbucket.org',
+        user: 'git',
+        identityFile: '~/.ssh/id_rsa',
+      },
+    ]
+  )
   action :create
 end
 
@@ -33,10 +56,12 @@ end
 node['git']['github_repositories'].each do |repository|
   git "github:#{node['git']['username']}/#{repository}" do
     checkout_branch 'master'
-    enable_checkout false
+    revision 'master'
     destination "C:\\dev\\#{node['git']['username']}\\#{repository}"
     repository "git@github.com:#{node['git']['username']}/#{repository}.git"
     timeout node['git']['timeout']
+    enable_checkout false
+    enable_submodules true
     action :checkout
   end
 end
@@ -44,9 +69,12 @@ end
 node['git']['gitlab_repositories'].each do |repository|
   git "gitlab:#{node['git']['username']}/#{repository}" do
     checkout_branch 'master'
+    revision 'master'
     destination "C:\\dev\\#{node['git']['username']}\\#{repository}"
     repository "git@gitlab.com:#{node['git']['username']}/#{repository}.git"
     timeout node['git']['timeout']
+    enable_checkout false
+    enable_submodules true
     action :checkout
   end
 end
